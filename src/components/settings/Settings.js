e42d57df6e1bc2f27beb1200098db29a84b19d70
Marvel Dev <marvellousalika48@gmail.com>
2021-04-03 07:54:15 +0100

    add design for settings-component

--- src/components/settings/Settings.js
+++ src/components/settings/Settings.js
@@ -16,42 +16,44 @@ const Settings = () => {
         <button>Switch to Professional Account</button>
       </div>
       <div className="settings-pane">
-        <span className="user-info">
+        <div className="user-profile">
           <img src={CONSTANTS.PHOTOURL} className="nav-photo" alt="profile" />
           <div>
             <p>Add Name</p>
-            <button>Change Profile Photo</button>
+            <button id="photoButton">Change Profile Photo</button>
           </div>
-        </span>
-        <label>
-          Name
-          <input type="text"/>
-        </label>
-        <label>
-          Username
-          <input type="text"/>
-        </label>
-        <label>
-          Website
-          <input type="text"/>
-        </label>
-        <label>
-          Bio
-          <textarea name="" id="" cols="30" rows="2"></textarea>
-        </label>
-        <label>
-          Email
-          <input type="email"/>
-        </label>
-        <label>
-          Phone Number
-          <input type="text"/>
-        </label>
-        <label>
-          Gender
-          <input type="text"/>
-        </label>
-        <button>Submit</button>
+        </div>
+        <form>
+          <label>
+            Name
+            <input type="text" placeholder="Name" />
+          </label>
+          <label>
+            Username
+            <input type="text" placeholder="Username" />
+          </label>
+          <label>
+            Website
+            <input type="text" placeholder="Website" />
+          </label>
+          <label>
+            Bio
+            <textarea className="bio-box" placeholder="Bio"></textarea>
+          </label>
+          <label>
+            Email
+            <input type="email" placeholder="Email" />
+          </label>
+          <label>
+            Phone Number
+            <input type="text" placeholder="Phone Number" />
+          </label>
+          <label>
+            Gender
+            <input type="text" placeholder="Gender" />
+          </label>
+          <button id="submitButton">Submit</button>
+        </form>
       </div>
     </div>
   )
