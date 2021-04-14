0c59e8a1a71f979ebf8ef28cfc19d7b0cb20e60d
Marvel Dev <marvellousalika48@gmail.com>
2021-04-14 16:31:04 +0100

    fix bug with display next gallery function.
    - add display of previous gallery function

--- src/components/singlePost/SinglePost.js
+++ src/components/singlePost/SinglePost.js
@@ -4,29 +4,41 @@ import { CONSTANTS } from '../common/constants'
 
 const SinglePost = ({ setIsSinglePostOpen, selectedPostIndex }) => {
   const { bio } = useSelector(state => state.bio)
-  const galleryState = useSelector(state => state.gallery)
-  const [galleryPost, setGalleryPost] = useState(galleryState.gallery[selectedPostIndex])
+  const { gallery } = useSelector(state => state.gallery)
+  const [index, setIndex] = useState(selectedPostIndex)
 
-  const nextGalleryItem = event => {
+  const displayNextGallery = event => {
     event.stopPropagation()
-    selectedPostIndex++
-    selectedPostIndex = selectedPostIndex % galleryState.gallery.length
-    setGalleryPost(galleryState.gallery[selectedPostIndex])
-    console.log(selectedPostIndex);
+    setIndex(index + 1)
+    if (index === gallery.length - 1) {
+      setIndex(0)
+    }
+  }
+
+  const displayPreviousGallery = event => {
+    event.stopPropagation()
+    setIndex(index - 1)
+    if (index === 0) {
+      setIndex(gallery.length -1)
+    }
   }
 
   return (
     <>
       <div className="overlay" onClick={() => setIsSinglePostOpen(false)}>
-        <button className="remove button"><i className="material-icons">&#xe5cd;</i></button>
-        <button className="previous button"><i className="fa fa-angle-left"></i></button>
-        <button onClick={nextGalleryItem} className="next button">
+        <button className="remove button">
+          <i className="material-icons">&#xe5cd;</i>
+        </button>
+        <button onClick={displayPreviousGallery} className="previous button">
+          <i className="fa fa-angle-left"></i>
+        </button>
+        <button onClick={displayNextGallery} className="next button">
           <i className="fa fa-angle-right"></i>
         </button>
       </div>
       <div className="single-post">
         <div className="post-photo">
-          <img src={galleryPost.photoUrl} alt="gallery"/>
+          <img src={gallery[index].photoUrl} alt="gallery"/>
         </div>
         <div>
           <div className="user-info">
@@ -43,7 +55,7 @@ const SinglePost = ({ setIsSinglePostOpen, selectedPostIndex }) => {
               />
               <div>
                 <span className="bio-name">{bio?.username || CONSTANTS.NAME}</span>
-                <span id="caption">{galleryPost.photoCaption}</span>
+                <span id="caption">{gallery[index].photoCaption}</span>
               </div>
             </div>
           </div>
