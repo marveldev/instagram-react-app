35dc554e1eb4730f9ea6ed867bbc7f45575d7e61
Marvel Dev <marvellousalika48@gmail.com>
2021-04-25 13:02:08 +0100

    remove auto increment of gallery id.

--- src/database.js
+++ src/database.js
@@ -4,7 +4,7 @@ const database = new Dexie('Instagram')
 
 database.version(1).stores({
   bio: '++id,name,website,aboutUser,email,PhoneNumber,gender,profilePhoto',
-  gallery: '++id,photoCaption,photoUrl'
+  gallery: 'id,photoCaption,photoUrl'
 })
 
 export default database
