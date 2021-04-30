4a4fc67af1d818112bab8498985eb9444b79ac94
Marvel Dev <marvellousalika48@gmail.com>
2021-04-30 09:34:35 +0100

    add mobile single post feature

--- src/components/singlePost/MobileSinglePost.jsx
+++ src/components/singlePost/MobileSinglePost.jsx
@@ -1,54 +1,60 @@
 import { useSelector, useDispatch } from 'react-redux'
+import { useHistory } from 'react-router'
 import { CONSTANTS } from '../../common/constants'
+import './mobileSinglePost.scss'
 
-const MobileSinglePost = ({ setCurrentPostPage, selectedPostIndex, setSelectedPostIndex }) => {
+const MobileSinglePost = () => {
+  const { goBack } = useHistory()
   const { bio } = useSelector(state => state.bio)
   const galleryState = useSelector(state => state.gallery)
-  const { posts, comments } = galleryState
+  const { posts, comments, selectedPostIndex } = galleryState
 
   return (
     <>
       <div className="mobile-single-post">
         <div className="header">
-          <button>back</button>
+          <button onClick={goBack} className="material-icons">&#xe5c4;</button>
           <span>Posts</span>
         </div>
-        <div  className="content">
-          <div className="user-info">
-            <img src={bio?.profilePhotoUrl || CONSTANTS.PHOTOURL}
-              className="nav-photo" alt="profile"
-            />
-            <span className="bio-name">{bio?.username || CONSTANTS.NAME}</span>
-            <button>
-              <i className="material-icons">&#xe5d3;</i>
-            </button>
-          </div>
-          <div className="post-photo">
-            <img src={{}} alt="gallery"/>
-          </div>
-          <div>
-            <div className="single-post-options">
-              <div>
-                <button className="fa fa-heart-o"></button>
-                <button className="fa fa-comment-o"></button>
-                <button className="fa fa-share-square-o"></button>
-              </div>
-              <button className="material-icons">&#xe867;</button>
-            </div>
-            <span><strong>{0}</strong> Likes</span>
-            <span><strong>{0}</strong> Comments</span>
-            {/* {posts[selectedPostIndex].photoCaption && (
-              <div className="caption-info">
+        <div className="content">
+          {posts?.map((post) => (
+            <div key={post.id}>
+              <div className="user-info">
                 <img src={bio?.profilePhotoUrl || CONSTANTS.PHOTOURL}
                   className="nav-photo" alt="profile"
                 />
+                <span className="bio-name">{bio?.username || CONSTANTS.NAME}</span>
+                <button>
+                  <i className="material-icons">&#xe5d4;</i>
+                </button>
+              </div>
+              <div className="post-photo">
+                <img src={post.photoUrl} alt="gallery"/>
+              </div>
+              <div className="post-details">
+                <div className="single-post-options">
+                  <div>
+                    <button className="fa fa-heart-o"></button>
+                    <button className="fa fa-comment-o"></button>
+                    <button className="fa fa-share-square-o"></button>
+                  </div>
+                  <button className="material-icons">&#xe867;</button>
+                </div>
+                <span><strong>{post.likesCount || 0}</strong> Likes</span>
+                <span><strong>{0}</strong> Comments</span>
+                {post.photoCaption && (
+                  <div className="caption-info">
+                    <span className="bio-name">{bio?.username || CONSTANTS.NAME}</span>
+                    <span>{posts[selectedPostIndex].photoCaption}</span>
+                  </div>
+                )}
                 <div>
-                  <span className="bio-name">{bio?.username || CONSTANTS.NAME}</span>
-                  <span className="text">{posts[selectedPostIndex].photoCaption}</span>
+                  <button>View all comments</button>
+                  <div>Last two comments</div>
                 </div>
               </div>
-            )} */}
-          </div>
+            </div>
+          ))}
         </div>
       </div>
     </>
